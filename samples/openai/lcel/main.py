bd9b54ba72ed34fc557938f2a606966b82e06daf
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2024-06-23 12:59:40 -0300

    Adds LCEL class and adds a sample class for OpenAI

---
+++ samples/openai/lcel/main.py
@@ -0,0 +1,32 @@
+import os
+import logging
+from uuid import uuid4
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from dialog_lib.agents.openai import DialogLCELOpenAI
+
+logging.getLogger().setLevel(logging.ERROR)
+
+database_url = "postgresql://talkdai:talkdai@db:5432/test_talkdai"
+
+engine = create_engine(database_url)
+
+dbsession = Session(engine)
+
+
+agent = DialogLCELOpenAI(
+    model="gpt-4o",
+    temperature=0.1,
+    llm_api_key=os.environ.get("OPENAI_API_KEY"),
+    prompt="You are a bot called Sara. Be nice to other human beings.",
+    dbsession=dbsession,
+    config={
+        "database_url": database_url,
+    },
+    session_id=str(uuid4())
+)
+
+while True:
+    input_text = input("You: ")
+    output_text = agent.process(input_text)
+    print(f"Sara: {output_text}")
