5611a87adf0df0f51b5ddfa7084f7843d868e794
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2024-07-01 22:10:30 -0300

    Fixes comments from @lgabs

--- dialog_lib/agents/openai.py
+++ dialog_lib/agents/openai.py
@@ -1,5 +1,5 @@
 import os
-from .abstract import AbstractDialog, AbstractLCELDialog
+from .abstract import AbstractDialog, AbstractLCEL
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai.chat_models.base import ChatOpenAI
 from dialog_lib.embeddings.retrievers import DialogRetriever
@@ -20,7 +20,7 @@ class DialogOpenAI(AbstractDialog):
         return output.get("text")
 
 
-class DialogLCELOpenAI(AbstractLCELDialog):
+class DialogLCELOpenAI(AbstractLCEL):
     def __init__(self, *args, **kwargs):
         self.openai_api_key = kwargs.get("llm_api_key") or os.environ.get("OPENAI_API_KEY")
         kwargs["model_class"] = ChatOpenAI(
